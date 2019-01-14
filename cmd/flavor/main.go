fef547d6dc03101b8ddc997379a6a882b0f316d2
Salonee Rege <salonee.rege@intel.com>
2019-01-14 15:27:12 -0800

    Input validation added for GetImageFlavor parameters

--- cmd/flavor/main.go
+++ cmd/flavor/main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"intel/isecl/lib/flavor"
 	"log"
+	"net/url"
 	"os"
+	"regexp"
 	"strconv"
 )
 
@@ -18,11 +20,24 @@ func main() {
 	switch methodName {
 
 	case "GetImageFlavor":
-		log.Println("Image flavor creation method called")
 		if len(os.Args[1:]) < 5 {
 			log.Fatal("Usage :  ./lib-flavor GetImageFlavor label encryptionRequired keyURL digest")
 		}
-		encryptionRequired, _ := strconv.ParseBool(os.Args[3])
+
+		encryptionRequired, err := strconv.ParseBool(os.Args[3])
+		if err != nil {
+			log.Fatal("Invalid input : encryptionRequired must be a boolean value. ")
+		}
+		_, err = url.ParseRequestURI(os.Args[4])
+		if err != nil {
+			log.Fatal("Invalid input : keyURL ")
+		}
+
+		if !isValidDigest(os.Args[5]) {
+			log.Fatal("Invalid input : digest must be a hexadecimal value and 64 characters in length.")
+		}
+
+		log.Println("Image flavor creation method called")
 		imageFlavor, err := flavor.GetImageFlavor(os.Args[2], encryptionRequired, os.Args[4], os.Args[5])
 		if err != nil {
 			log.Println(err)
@@ -36,4 +51,10 @@ func main() {
 	default:
 		log.Println("Invalid method name. \nExpected values: GetImageFlavor")
 	}
+}
+
+//isValidDigest method checks if the digest value is hexadecimal and 64 characters in length
+func isValidDigest(value string) bool {
+	r := regexp.MustCompile("^[a-fA-F0-9]{64}$")
+	return r.MatchString(value)
 }
