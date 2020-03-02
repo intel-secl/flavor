d5a0eb454312cade93e9757fbe9085023baf45d5
arvind5 <arvind.rawat@intel.com>
2020-03-02 08:05:14 -0800

    Removed internal urls.

--- container_image_flavor_test.go
+++ container_image_flavor_test.go
@@ -15,8 +15,8 @@ import (
 func TestContainerImageFlavorCreation(t *testing.T) {
 
 	//flavor with encryption enabled and integrity enforced
-	flavorInput, err := GetContainerImageFlavor("10.105.168.101:5000/hello-world:encrypted", true,
-		"http://10.1.68.21:20080/v1/keys/73755fda-c910-46be-821f-e8ddeab189e9/transfer", true, "https://10.105.168.141:4443")
+	flavorInput, err := GetContainerImageFlavor("docker.registry.com:5000/hello-world:encrypted", true,
+		"http://kbs.server.com:20080/v1/keys/73755fda-c910-46be-821f-e8ddeab189e9/transfer", true, "https://docker.notary.com:4443")
 	if err != nil {
 		log.Println(err)
 	}
@@ -26,8 +26,8 @@ func TestContainerImageFlavorCreation(t *testing.T) {
 	assert.NotNil(t, flavor)
 
 	//Flavor with only integrity enforced
-	flavorInput, err = GetContainerImageFlavor("10.105.168.101:5000/hello-world:signed", false, "",
-		true, "https://10.105.168.141:4443")
+	flavorInput, err = GetContainerImageFlavor("docker.registry.com:5000/hello-world:signed", false, "",
+		true, "https://docker.notary.com:4443")
 	if err != nil {
 		log.Println(err)
 	}
@@ -36,8 +36,8 @@ func TestContainerImageFlavorCreation(t *testing.T) {
 	assert.NotNil(t, flavor)
 
 	//Flavor with only encryption
-	flavorInput, err = GetContainerImageFlavor("10.105.168.101:5000/hello-world:encrypted", true,
-		"http://10.1.68.21:20080/v1/keys/73755fda-c910-46be-821f-e8ddeab189e9/transfer", false, "")
+	flavorInput, err = GetContainerImageFlavor("docker.registry.com:5000/hello-world:encrypted", true,
+		"http://kbs.server.com:20080/v1/keys/73755fda-c910-46be-821f-e8ddeab189e9/transfer", false, "")
 	if err != nil {
 		log.Println(err)
 	}
--- image_flavor_test.go
+++ image_flavor_test.go
@@ -12,7 +12,7 @@ import (
 
 func TestImageFlavorCreationWithEncryption(t *testing.T) {
 	flavorInput, err := GetImageFlavor("Cirros-Enc-Label", true,
-		"http://10.1.68.21:20080/v1/keys/73755fda-c910-46be-821f-e8ddeab189e9/transfer",
+		"http://kbs.server.com:20080/v1/keys/73755fda-c910-46be-821f-e8ddeab189e9/transfer",
 		"261209df1789073192285e4e408addadb35068421ef4890a5d4d434")
 	assert.NoError(t, err)
 	flavor, err := json.Marshal(flavorInput)
